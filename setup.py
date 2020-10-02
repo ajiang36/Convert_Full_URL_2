04f3bbf9d91fe5fa5a9e04ee223b3a177508aaf4
Amy Jiang <71291951+ajiang36@users.noreply.github.com>
2020-10-02 00:25:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-ajiang36",
+    name="full_URL-ajiang36",
+    packages=["full_URL-ajiang36"],
     version="0.0.1",
+    license="MIT",
+    description="This package converts partial URLs to full URLs",
     author="Amy Jiang",
     author_email="ajiang21@andover.edu",
-    license="MIT",
-    description="This package converts partial urls into full urls",
+    url="https://github.com/ajiang36/Convert_Full_URL_2.git",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
@@ -18,6 +20,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
     ],
     python_requires='>=3.6',
 )
